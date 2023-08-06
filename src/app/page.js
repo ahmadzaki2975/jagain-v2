3f233ccb93235f6c1ca1a515ba184a5f8bc52a67
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-06 18:33:03 +0700

    fix: improve responsiveness of main page

--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       </nav>
 
       <section className="flex text-center mt-20 //min-h-screen z-[1] text-blue-primary font-montserrat font-bold flex-col justify-center items-center">
-        <p className="text-[60px] leading-[125%]">
+        <p className="text-[40px] sm:text-[45px] lg:text-[60px] leading-[125%]">
           Easily manage <br /> <span className="text-grey-primary">your</span>{" "}
           personal finances
         </p>
@@ -32,20 +32,20 @@ export default function Home() {
           <Button text="Sign In" variant="secondary" classname="px-10 py-4" onClick={() => router.push("/auth")} />
           <Button text="Register" variant="primary" classname="px-10 py-4" />
         </div>
-        <div className="grid grid-cols-4 w-fit mx-auto gap-10 mt-[100px]">
-          <div className="bg-blue-primary/20 text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
+        <div className="grid grid-cols-2 lg:grid-cols-4 w-fit mx-auto gap-5 lg:gap-10 mt-[100px]">
+          <div className="bg-blue-primary/20 grid place-items-center text-[16px] md:text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
             pharetra sem sit amet
           </div>
-          <div className="bg-blue-primary/20 text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
+          <div className="bg-blue-primary/20 grid place-items-center text-[16px] md:text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
             pharetra sem sit amet
           </div>
-          <div className="bg-blue-primary/20 text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
+          <div className="bg-blue-primary/20 grid place-items-center text-[16px] md:text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
             pharetra sem sit amet
           </div>
-          <div className="bg-blue-primary/20 text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
+          <div className="bg-blue-primary/20 grid place-items-center text-[16px] md:text-[18px] text-grey-primary font-semibold px-[15px] py-[10px] rounded-[11px] w-[230px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
             pharetra sem sit amet
           </div>
