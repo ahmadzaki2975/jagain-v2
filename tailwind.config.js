afc56367159859677a111e9700699bd61a3877ac
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-09 19:15:07 +0700

    feat(major): re-initialize Next without app directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        blue: {
-          primary: "#628FFE",
-          secondary: "#769DFC",
-        },
-        grey: {
-          primary: "#5D5D5D",
-        }
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily: {
-        montserrat: "'Montserrat', sans-serif",
-      },
-      screens: {
-        xs: "425px",
-      }
     },
   },
   plugins: [],
