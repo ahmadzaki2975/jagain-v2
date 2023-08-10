3824108de033324413ae13826cbc407e3cc1ee16
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-10 17:05:08 +0700

    feat: define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue: {
+          primary: "#628FFE",
+          secondary: "#769DFC",
+        },
+        grey: {
+          primary: "#5D5D5D",
+        },
+      },
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+      },
+      screens: {
+        xs: "425px",
       },
     },
   },
   plugins: [],
-}
+};
