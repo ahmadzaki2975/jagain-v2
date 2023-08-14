dc71331905e9a7b7e05c0149b46e7d299b523225
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-14 15:54:34 +0700

    fix: improve responsiveness layout-wise
    - To do: font size responsiveness

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <main className="min-h-screen font-montserrat flex justify-center items-center px-[5%] py-[5%]">
-      <section className="grid w-1/2 place-items-center">
+    <main className="min-h-screen py-10 lg:py-0 font-montserrat flex flex-col gap-10 lg:gap-0 lg:flex-row justify-center items-center px-[5%] py-[5%]">
+      <section className="grid w-[90%] xs:w-[80%] max-w-[600px] 2xl:max-w-[800px] lg:w-1/2 place-items-center">
         <div className="flex gap-4 ">
           <img src="/Logo.png" alt="Logo" className="w-[42px] h-[97px]" />
           <h1 className="text-[56px] font-semibold text-blue-secondary">
@@ -12,7 +12,7 @@ export default function LoginPage() {
         </div>
       </section>
 
-      <section className="w-1/2 h-fit text-grey-primary shadow-[0_0_25px_#E0E9FF] p-8 rounded-[15px]">
+      <section className="w-[90%] xs:w-[70%] lg:w-1/2 max-w-[600px] 2xl:max-w-[700px] h-fit text-grey-primary shadow-[0_0_25px_#E0E9FF] p-8 rounded-[15px]">
         <h1 className="font-semibold text-[35px]">Sign In</h1>
         <p className="font-semibold">
           New user?{" "}
@@ -32,11 +32,11 @@ export default function LoginPage() {
             type="text"
             className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
           />
-          <label className="mt-5" htmlFor="email">Password</label>
+          {/* <label className="mt-5" htmlFor="email">Password</label>
           <input
             type="text"
             className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
-          />
+          /> */}
           <button
             type="submit"
             className="bg-blue-primary w-fit px-4 self-end text-white font-semibold rounded-[8px] py-2 mt-5"
