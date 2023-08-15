d040eb1e78bd3a24f1a629a73c859518d8e24bea
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-15 18:33:19 +0700

    feat: now display login form in 2 steps

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+
   return (
     <main className="min-h-screen py-10 lg:py-0 font-montserrat flex flex-col gap-10 lg:gap-0 lg:flex-row justify-center items-center px-[5%]">
       <section className="grid w-[90%] xs:w-[80%] max-w-[600px] 2xl:max-w-[800px] lg:w-1/2 place-items-center">
@@ -24,19 +29,42 @@ export default function LoginPage() {
           className="flex flex-col mt-10"
           onSubmit={(e) => {
             e.preventDefault();
-            alert("Test");
+            if (email == "") {
+              if (emailValue == "") {
+                alert("Please enter your email address");
+                return;
+              }
+              setEmail(emailValue);
+              return;
+            } else {
+              alert("log in");
+            }
           }}
         >
-          <label htmlFor="email">Email Address</label>
+          <label className={email != "" ? " hidden" : " block"} htmlFor="email">
+            Email Address
+          </label>
           <input
             type="text"
-            className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
+            className={
+              "py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none" +
+              (email != "" ? " hidden" : " block")
+            }
+            value={emailValue}
+            onChange={(e) => {
+              setEmailValue(e.target.value);
+            }}
           />
-          {/* <label className="mt-5" htmlFor="email">Password</label>
+          <label className={email == "" ? " hidden" : " block"} htmlFor="email">
+            Password
+          </label>
           <input
             type="text"
-            className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
-          /> */}
+            className={
+              "py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none " +
+              (email == "" ? " hidden" : " block")
+            }
+          />
           <button
             type="submit"
             className="bg-blue-primary w-fit px-4 self-end text-white font-semibold rounded-[8px] py-2 mt-5"
@@ -52,7 +80,9 @@ export default function LoginPage() {
         <div className="py-3 unselectable hover:bg-[#D7D7D7]/60 w-full cursor-pointer text-center mt-5 rounded-[9px] border-[.5px] border-solid border-[#D7D7D7]">
           Continue With Google
         </div>
-        <Link href="#" className="block mt-10 text-blue-primary font-semibold">Need help?</Link>
+        <Link href="#" className="block mt-10 text-blue-primary font-semibold">
+          Need help?
+        </Link>
       </section>
     </main>
   );
