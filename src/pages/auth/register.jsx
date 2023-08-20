c799924431b84d23fa723965068845ca1e1641f0
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-08-20 13:55:23 +0700

    feat: add input placeholders

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import { useState } from "react";
 
-export default function AuthPage() {
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+
   return (
-    <main className="min-h-screen font-montserrat flex justify-center items-center px-[5%] py-[5%]">
-      <section className="grid w-1/2 place-items-center">
+    <main className="min-h-screen py-10 lg:py-0 font-montserrat flex flex-col gap-10 lg:gap-0 lg:flex-row justify-center items-center px-[5%]">
+      <section className="grid w-[90%] xs:w-[80%] max-w-[600px] 2xl:max-w-[800px] lg:w-1/2 place-items-center">
         <div className="flex gap-4 ">
           <img src="/Logo.png" alt="Logo" className="w-[42px] h-[97px]" />
           <h1 className="text-[56px] font-semibold text-blue-secondary">
@@ -12,30 +17,59 @@ export default function AuthPage() {
         </div>
       </section>
 
-      <section className="w-1/2 h-fit text-grey-primary shadow-[0_0_25px_#E0E9FF] p-8 rounded-[15px]">
-        <h1 className="font-semibold text-[35px]">Sign In</h1>
-        <p className="font-semibold">
+      <section className="w-[90%] xs:w-[70%] lg:w-1/2 max-w-[600px] 2xl:max-w-[700px] h-[90vh] overflow-y-scroll pt-10 pb-20 text-grey-primary shadow-[0_0_25px_#E0E9FF] p-8 rounded-[15px]">
+        <h1 className="font-semibold text-[35px]">
+          Register your account
+        </h1>
+        {/* <p className={"font-semibold" + (email == "" ? " block" : " hidden")}>
           New user?{" "}
           <Link href="/register" className="text-blue-primary">
             Register an account
           </Link>
-        </p>
+        </p> */}
         <form
           className="flex flex-col mt-10"
           onSubmit={(e) => {
             e.preventDefault();
-            alert("Test");
           }}
         >
-          <label htmlFor="email">Email Address</label>
+          <label className={"block font-semibold"} htmlFor="email">
+            Email Address
+          </label>
           <input
             type="text"
-            className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
+            className={
+              "py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
+            }
+            value={emailValue}
+            onChange={(e) => {
+              setEmailValue(e.target.value);
+            }}
+            placeholder="example@me.com"
           />
-          <label className="mt-5" htmlFor="email">Password</label>
+          <label className="block font-semibold mt-7" htmlFor="phone">
+            Phone Number
+          </label>
           <input
+            name="phone"
+            id="phone"
             type="text"
-            className="//bg-red-500 py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
+            className={
+              "py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none"
+            }
+            placeholder="+6281234567890"
+          />
+          <label className={"block font-semibold mt-7"} htmlFor="password">
+            Password
+          </label>
+          <input
+            name="password"
+            id="password"
+            type="text"
+            className={
+              "py-1 border-b-[#D7D7D7] border-b-[.5px] focus:border-b-[#15181b]/90 !outline-none "
+            }
+            placeholder="********"
           />
           <button
             type="submit"
@@ -52,7 +86,6 @@ export default function AuthPage() {
         <div className="py-3 unselectable hover:bg-[#D7D7D7]/60 w-full cursor-pointer text-center mt-5 rounded-[9px] border-[.5px] border-solid border-[#D7D7D7]">
           Continue With Google
         </div>
-        <Link href="#" className="block mt-10 text-blue-primary font-semibold">Need help?</Link>
       </section>
     </main>
   );
